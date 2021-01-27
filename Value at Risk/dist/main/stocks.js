718be8b41503a640317ccb05eca87fb82774813d
Kjubie Bruckner <abru1322@gmail.com>
2021-01-27 14:25:38 +0100

    Builded to exe

---
+++ Value at Risk/dist/main/stocks.js
@@ -0,0 +1,10 @@
+//format: symbol, Name
+var stocks = [
+["NDAQ", "Nasdaq, Inc. (NDAQ)"],
+["TSLA", "Tesla, Inc. (TSLA)"],
+["AAPL", "Apple (AAPL)"],
+["SNOW", "Snowflake Inc. (SNOW)"],
+["SPOT", "Spotify Technology S.A. (SPOT)"],
+["ZM", "Zoom Video Communications, Inc. (ZM)"],
+["TEST", "TEST ADD"],
+]
--- Value at Risk/stocks.js
+++ Value at Risk/stocks.js
@@ -6,7 +6,4 @@ var stocks = [
 ["SNOW", "Snowflake Inc. (SNOW)"],
 ["SPOT", "Spotify Technology S.A. (SPOT)"],
 ["ZM", "Zoom Video Communications, Inc. (ZM)"],
-["TEST1", "TEST NUMMER 1"],
-["TEST2", "TEST NUMMER 2"],
-["TEST3", "TEST NUMMER 3"],
 ]
